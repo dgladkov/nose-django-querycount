85a94a6301cca3c1ed5b6506f422d7ba134a7c46
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2011-03-15 05:11:56 +0200

    subclass ColoredString from unicode

--- nosequerycount/utils.py
+++ nosequerycount/utils.py
@@ -1,8 +1,8 @@
-class ColoredString(str):
+class ColoredString(unicode):
     """
     Utility string class for colored terminal output
     """
     BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
 
     def __new__(cls, seq, color):
-        return str.__new__(cls, '\033[1;%dm%s\033[0m' % (30 + color, seq))
+        return unicode.__new__(cls, '\033[1;%dm%s\033[0m' % (30 + color, seq))
